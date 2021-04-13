78d0879270ca886956d9ad736e0d9e9b8c409ade
GyeongJinMin <mkj0121@naver.com>
2021-04-13 22:03:54 +0900

    add sayhi

--- src/main/java/kr/ac/hansung/cse/hellospringboot/controller/HelloWorldController.java
+++ src/main/java/kr/ac/hansung/cse/hellospringboot/controller/HelloWorldController.java
@@ -1,6 +1,7 @@
 package kr.ac.hansung.cse.hellospringboot.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -8,6 +9,11 @@ public class HelloWorldController {
 
     @RequestMapping("/")
     public String sayHello() {
+        return "index";
+    }
+
+    @GetMapping("/hi")
+    public String sayHi() {
         return "index";
     }
 }
